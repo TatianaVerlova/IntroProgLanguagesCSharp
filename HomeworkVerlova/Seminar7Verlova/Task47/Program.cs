9a7d299f1089ddb3f89f1eb4089d5c39eabf314d
Tatiana Verlova <tatiana.verlova@gmail.com>
2022-08-17 01:01:38 +0300

    add homework to the 7 seminar

---
+++ HomeworkVerlova/Seminar7Verlova/Task47/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] GetArray(int rows, int columns)
+{
+    double[,] array = new double[rows, columns];
+    double number;
+    Random rnd = new Random();
+
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            number = rnd.NextDouble()*100 - 50;
+            array[i,j] = Math.Round(number, 2);
+        }
+    }
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк и столбцов через пробел: ");
+string[] input = Console.ReadLine().Split();
+int row = int.Parse(input[0]);
+int col = int.Parse(input[1]);
+
+PrintArray(GetArray(row, col));
---
+++ HomeworkVerlova/Seminar7Verlova/Task52/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце. Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] GetArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    Random rnd = new Random();
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = rnd.Next(0,10);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк и столбцов через пробел: ");
+string[] input = Console.ReadLine().Split();
+int row = int.Parse(input[0]);
+int col = int.Parse(input[1]);
+Console.WriteLine();
+
+int[,] array = GetArray(row, col);
+PrintMatrix(array);
+Console.WriteLine();
+
+for(int i = 0; i < array.GetLength(1); i++)
+{
+    int sum = 0;
+    for(int j = 0; j < array.GetLength(0); j++)
+    {
+        sum += array[j,i];
+    }
+    double averageCol = (double) sum / col;
+    Console.Write(Math.Round(averageCol, 1) + "; ");
+}
