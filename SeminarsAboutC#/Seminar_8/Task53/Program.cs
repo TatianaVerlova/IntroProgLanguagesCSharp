a30c6a081851231088a21a62b0b5f5c8cb0b62a9
Tatiana Verlova <tatiana.verlova@gmail.com>
2022-08-18 00:16:33 +0300

    add seminar 8

---
+++ SeminarsAboutC#/Seminar_8/Task53/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+// 1 2 3 4    3 4 5 6
+// 5 6 7 8 -> 5 6 7 8
+// 9 0 1 2    9 0 1 2
+// 3 4 5 6    1 2 3 4
+
+void PrintArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Swap(int[,] array, int i, int j)
+{
+    int temp = array[i, j];
+    array[i, j] = array[array.GetLength(0) - 1, j];
+    array[array.GetLength(0) - 1, j] = temp;
+}
+
+int[,] myArray = {
+    { 1, 2, 3, 4 },
+    { 5, 6, 7, 8 },
+    { 9, 0, 1, 2 },
+    { 3, 4, 5, 6 }
+};
+
+for(int i = 0; i < myArray.GetLength(1); i++)
+{
+    Swap(myArray, 0, i);
+}
+
+PrintArray(myArray);
---
+++ SeminarsAboutC#/Seminar_8/Task55/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это
+// невозможно, программа должна вывести сообщение для пользователя.
+// 1 2 3 4    1 5 9 3
+// 5 6 7 8 -> 2 6 0 4
+// 9 0 1 2    3 7 1 5
+// 3 4 5 6    4 8 2 6
+
+int[,] myArray = {
+    { 1, 2, 3, 4 },
+    { 5, 6, 7, 8 },
+    { 9, 0, 1, 2 },
+    { 3, 4, 5, 6 }
+};
+
+if(myArray.GetLength(0) != myArray.GetLength(1))
+{
+    Console.WriteLine("Матрица не квадратная. Транспонировать не удастся");
+    return;
+}
+
+PrintArray(Transpose(myArray));
+
+int[,] Transpose (int[,] array)
+{
+    int[,] result = new int[array.GetLength(0), array.GetLength(1)];
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            result[i,j] = array[j,i];
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
