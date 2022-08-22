92eb4043688342bbf08d9212baac8ac6740d9d50
Tatiana Verlova <tatiana.verlova@gmail.com>
2022-08-22 23:41:13 +0300

    add seminar 9

---
+++ SeminarsAboutC#/Seminar_9/Task65/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 65: Задайте значения M и N. Напишите функцию, которая выведет все натуральные числа в промежутке от M до N от большего к меньшему.
+// M = 1; N = 5. -> "1, 2, 3, 4, 5"
+// M = 4; N = 8. -> "4, 5, 6, 7, 8"
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write(Naturalback(n, ""));
+
+string Naturalback(int start, string res)
+{
+    if(start == 0)
+    {
+        return res;
+    }
+
+    return($"{start} \t {Naturalback(--start, res)}");
+}
---
+++ SeminarsAboutC#/Seminar_9/Task69/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B
+// с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.WriteLine("Введите два числа через пробел: ");
+string[] input = Console.ReadLine().Split();
+int a = int.Parse(input[0]);
+int b = int.Parse(input[1]);
+
+Console.WriteLine(Power(a, b));
+
+double Power(int n, int p)
+{
+    if (p == 1)
+    {
+        return n;
+    }
+    if (p == 0)
+    {
+        return 1;
+    }
+    if (p < 0)
+    {
+        return 1 / Power(n, p * (-1));
+    }
+
+    return n * Power(n, --p);
+}
